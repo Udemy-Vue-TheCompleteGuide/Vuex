e4e63afff4be0fd6cd0ae87dc1f4ef901091c87e
Marlon Fernandez <mfernandez@coadvantage.com>
2020-09-13 21:14:09 -0400

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,31 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <h2>Vuex - The global manage store</h2>
+    <Result></Result>
+    <Counter></Counter>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Result from "@/components/Result";
+import Counter from "@/components/Counter";
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Result,
+    Counter
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+/*#app {*/
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;*/
+/*  -webkit-font-smoothing: antialiased;*/
+/*  -moz-osx-font-smoothing: grayscale;*/
+/*  text-align: center;*/
+/*  color: #2c3e50;*/
+/*  margin-top: 60px;*/
+/*}*/
 </style>
